e956355257f37cf42d1f0e19c5f7fb6e86dd16bf
b4lie <1464576565@qq.com>
2026-06-02 20:30:23 +0800

    2.0.4

--- Runtime/UXComponent/UXHelper.cs
+++ Runtime/UXComponent/UXHelper.cs
@@ -1,3 +1,4 @@
+using AlicizaX;
 using UnityEngine;
 using AudioType = AlicizaX.Audio.Runtime.AudioType;
 
@@ -5,11 +6,13 @@ internal static class UXComponentExtensionsHelper
 {
     public static string GetString(string key)
     {
+        if (!AppServices.HasWorld) return key;
         return GameApp.Localization.GetString(key);
     }
 
     public static void PlayAudio(AudioClip clip)
     {
+        if (!AppServices.HasWorld) return;
         GameApp.Audio.Play(AudioType.UISound, clip);
     }
 }
